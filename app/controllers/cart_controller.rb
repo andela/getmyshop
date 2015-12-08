c66ffbed27fed7a0972a0ed4d7ecdc0cf0db6314
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2015-12-08 12:42:44 +0100

    Design Cart Preview Page
    Why?
    If a customer has added products to their cart, (s)he should be able to
    go to the cart page and see all the products in their cart.
    
    How?
    Generated the cart controller and create the view
    Design the cart preview page, rendering the 'cartproducts' partial
    Fix the rubocop and scss-lint style violations
    
    Completing feature "Design Cart Preview Page"
    https://www.pivotaltracker.com/story/show/109248944
    [finished #109248944]

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,20 @@
+class CartController < ApplicationController
+  def index
+    @cartproducts = [
+      { image: "http://i.imgur.com/QsgYAlQ.png",
+        text: "Sling-Back Stilletos",
+        price: "N5000"
+      },
+      {
+        image: "http://i.imgur.com/JQOY8u9.jpg",
+        text: "Beautiful Summer Hat",
+        price: "N1400"
+      },
+      {
+        image: "http://i.imgur.com/Pqlmpic.jpg",
+        text: "Brown Satchel Bag",
+        price: "N4600"
+      }
+    ]
+  end
+end

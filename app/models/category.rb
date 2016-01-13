59d6b97463640f407206faf62872209190aadeee
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-01-13 13:03:06 +0100

    Display shop products on landing page
    Why?
    Given that I am a customer
    When I visit the landing page of a shop
    I should see some featured products
    
    How?
    We fetched the product data from the databse
    using the relationship between product and category
    (A catergory has many products through subcategory)
    
    https://www.pivotaltracker.com/story/show/108000516
    [finishes #108000516]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :subcategories
-  has_many :products, through: :subcategory
+  has_many :products, through: :subcategories
   validates :name, presence: true
 end

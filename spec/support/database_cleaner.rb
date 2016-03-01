11ee9e2a06a1516725dc2d3b09d77b853646617d
olumuyiwa osiname <olumuyiwa.osiname@andela.com>
2016-03-01 11:10:48 +0100

    Set up testing tools for Integration testing
    Why?
    To enable testing pf features and units.
    
    How?
    Add database cleaner, capybara and selenium

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,22 +1,11 @@
+# DatabaseCleaner.strategy = :truncation
+
 # RSpec.configure do |config|
-
-#   config.before(:suite) do
-#     DatabaseCleaner.clean_with(:truncation)
-#   end
-
-#   config.before(:each) do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-
-#   config.before(:each, :js => true) do
-#     DatabaseCleaner.strategy = :truncation
-#   end
-
-#   config.before(:each) do
+#   config.use_transactional_fixtures = false
+#   config.before :each do
 #     DatabaseCleaner.start
 #   end
-
-#   config.after(:each) do
+#   config.after :each do
 #     DatabaseCleaner.clean
 #   end
 # end

c6f5aaa02f7903128bbb1e4ab4eff1eff9f67f48
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-02 11:56:11 +0100

    Test for order model
    Write test to assert the correct behaviour of the order Model
    
    How
    Generate factory for order model and set some values in it with faker
    Test the response to the order attributes.
    Test active model associations.
    Test acceptance of nested attributes for order items.
    
    https://www.pivotaltracker.com/story/show/114668271
    
    [finishes #114668271]
    
    Finishes test for order model
    
    Fixes rebase continue error
    
    Fixed code issues

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Order, type: :model do
-  let(:order) { build(:order) }
+  subject { build(:order) }
 
-  it "has a valid factory" do
-    expect(order).to be_valid
+  describe "has a valid factory" do
+    it { is_expected.to be_valid }
+  end
+
+  describe "instance methods" do
+    context "respond to instance method calls" do
+      it { is_expected.to respond_to(:order_number) }
+      it { is_expected.to respond_to(:payment_method) }
+      it { is_expected.to respond_to(:total_amount) }
+      it { is_expected.to respond_to(:user_id) }
+      it { is_expected.to respond_to(:address_id) }
+    end
+  end
+
+  describe "ActiveModel Relation" do
+    it { is_expected.to have_many(:order_items) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:address) }
+  end
+
+  describe "Nested Attribute" do
+    it { is_expected.to accept_nested_attributes_for(:order_items) }
   end
 end

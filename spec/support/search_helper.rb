1a694e923df7303c94686a7c78862c12fdc1588c
olumuyiwa osiname <olumuyiwa.osiname@andela.com>
2016-03-15 15:07:18 +0100

    Test search feature
    Test that a search returns products that meet the search criteria
    
    https://www.pivotaltracker.com/story/show/114657521
    
    [finishes #114657521]

---
+++ spec/support/search_helper.rb
@@ -0,0 +1,10 @@
+module SearchHelper
+  def fill_and_search(term)
+    visit root_path
+    expect(page).to have_css("#search", visible: true)
+    find("#search").click
+    fill_in("term", with: term)
+    find("#my-input-field").native.send_keys(:return)
+    expect(page).to have_css("#search", visible: true)
+  end
+end

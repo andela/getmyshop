ca2453e7344d7158dbff046011ebaba6e35a0703
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-06 16:28:36 +0100

    feat(delivery status): add logic for changing order status based on time order was created

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -18,6 +18,21 @@ module OrdersHelper
       render partial: "old_addresses"
     else
       render partial: "no_address"
+  end
+end
+
+  def tracking(t)
+    time = Time.now.to_i
+    if time.between?(t.to_i, (t + 4.hour).to_i)
+      "Order Created"
+    elsif time.between?((t + 4.hour).to_i, (t + 8.hour).to_i)
+        "Pending"
+    elsif time.between?((t + 8.hour).to_i, (t + 12.hour).to_i)
+      "Processing"
+    elsif time.between?((t + 12.hour).to_i, (t + 16.hour).to_i)
+      "Completed"
+    else
+      "Delivered"
     end
   end
 end

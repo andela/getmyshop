509afecdfcfef7381c8fb01866a516ab314d1fc6
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-12 17:28:53 +0100

    feat(product stock): a decorator method to display out of stock message when product is out of stock

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,17 +1,9 @@
 class ProductDecorator < Draper::Decorator
   delegate_all
 
-  def add_to_cart
-    if object.quantity > 0
-      h.submit_tag "ADD TO CART", class: "waves-effect btn products-input", id: "submit"
-    else
-      h.content_tag(:div, "Out of Stock", class: "out-of-stock")
-    end
-  end
-
   def stock
     if object.quantity > 0
-      "#{object.quantity} left in stock"
+      h.content_tag(:div, "#{object.quantity} left in stock", class: "stock-count")
     else
       h.content_tag(:div, "Out of Stock", class: "out-of-stock")
     end

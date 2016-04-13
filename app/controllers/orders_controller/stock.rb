69d6e922b28ca0051457fb76c840e093d1ebaa46
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-13 09:19:44 +0100

    style(hound issues): handle hound issues

--- app/controllers/orders_controller/stock.rb
+++ app/controllers/orders_controller/stock.rb
@@ -1,6 +1,5 @@
 class OrdersController
   class Stock
-
     def self.update(order)
       order.order_items.each do |order_item|
         product = Product.find(order_item.product_id)
@@ -8,6 +7,5 @@ class OrdersController
         product.save
       end
     end
-
   end
 end
---
+++ spec/controllers/orders_controller/stock_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe OrdersController::Stock do
+  @order = FactoryGirl.create(:order)
+
+  describe "#new" do
+    it "should create a new instance of stock" do
+      
+    end
+  end
+end

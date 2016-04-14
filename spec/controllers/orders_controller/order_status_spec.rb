d5c4d85840f8396f1aecd1c97b3e73506deaf0d1
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-14 11:45:15 +0100

    refactor(order status test): fix code review recommendations

--- spec/controllers/orders_controller/order_status_spec.rb
+++ spec/controllers/orders_controller/order_status_spec.rb
@@ -17,22 +17,6 @@ RSpec.describe OrdersController::OrderStatus do
       expect(@order.status).to eq("Pending")
     end
 
-    it "should have shipped status after 4 hours" do
-      new_order = create(:order)
-      new_order.created_at = Time.now - 5.hour
-      new_order.save
-      result = OrdersController::OrderStatus.new(new_order.user).save
-      expect(result[0].status).to eql("Shipped")
-    end
-
-    it "should have delivered status after 12 hours" do
-      new_order = create(:order)
-      new_order.created_at = Time.now - 13.hour
-      new_order.save
-      result = OrdersController::OrderStatus.new(new_order.user).save
-      expect(result[0].status).to eql("Delivered")
-    end
-
     context "when the hour range is between 4 and 12" do
       it "sets the order status to shipped" do
         new_order = create(:order)

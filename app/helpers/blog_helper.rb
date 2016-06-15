5710fbbd8fbf091406dc59ff0b890133a893bbfc
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-06-15 15:06:18 +0100

    chore(refactor): adherering to style houndci [delivers #120778559]

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -2,16 +2,16 @@ module BlogHelper
   def link_to_previous(post_id)
     previous_post_id = post_id - 1
     if previous_post_id > 0
-      link_to "Previous", "/blog/#{previous_post_id}", { :class => "btn left" }
+      link_to "Previous", "/blog/#{previous_post_id}", :class => "btn left"
     end
   end
 
   def link_to_next(post_id)
     next_post_id = post_id + 1
-    link_to "Next", "/blog/#{next_post_id}", { :class => "btn right" }
+    link_to "Next", "/blog/#{next_post_id}", :class => "btn right"
   end
 
   def link_to_post(id)
-    link_to "Read More...", "/blog/#{id}", { :class => "btn" }
+    link_to "Read More...", "/blog/#{id}", :class => "btn"
   end
 end

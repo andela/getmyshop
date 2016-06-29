e188d0c18d95fe2bfaf61bc86606295d127d70e6
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-06-29 14:26:35 +0100

    fix: failing shop controller test [#107709190]

--- spec/controllers/shop_controller_spec.rb
+++ spec/controllers/shop_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ShopController, type: :controller do
 	describe 'signup page' do
 	  context 'when user requests signup url' do
 	  	it 'returns a 200 status code' do
-	  		get :signup
+	  		get :new
 	  		expect(response.status).to eql(200)
 	  	end
 
 	  	it 'loads the signup page' do
-	  		get :signup
-	  		expect(response).to render_template(:200)
+	  		get :new
+	  		expect(response).to render_template(:new)
 	  	end
 	  end
 	end
